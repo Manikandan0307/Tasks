e3d9b968acfb2bbc1124bf6c51ad1da88820029a
Manikandan0307 <mrperfectboi789987@gmail.com>
2024-11-13 11:15:12 +0530

    Add files via upload

---
+++ String Based Problems/Program2.java
@@ -0,0 +1,11 @@
+package string.problems.com;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		System.out.println("enter a string:");
+		String s = System.console().readLine();
+	System.out.println(s.length());	
+	}
+
+}
